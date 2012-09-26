0830545f0d4e74683bc017c040aac7031dcf8062
riklomas <lomas.rik@gmail.com>
2012-09-26 08:29:24 +0100

    added lab work

---
+++ lab.rb
@@ -0,0 +1,6 @@
+# declare a hash representing the four suites of a deck of cards
+# with an abbreviation for the keys and a full name for the values
+
+
+# display the rank and suit for each card, as "9 of Hearts", etc.
+# don't worry about "jack" or "king" yet, just go up to "13 of Hearts", etc.

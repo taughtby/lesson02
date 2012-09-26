94c9e1379dc45960bf83dc5b609bce3c6af3297d
Darren Logue <dazzlerlogue@gmail.com>
2012-09-26 20:03:23 +0100

    answers to hashes

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,15 @@
 # 1. create an array with three different string values
+x = ['whats', 'the', 'score']
 
 # 2. add two new values to the Task #1 array in single expression
 
+x << ['QPR v', 'Reading']
+
 # 3. convert your name to an array, and display it, in a single expression
+
+name = ['darren', 'anthony','logue']
+puts name[0] + name[2]
 
 # 4. remove and display the last value in the Task # 1 array, and discuss
 # with the instructor why both values added in Task # 2 may have been removed
+puts x.shift
--- hashes.rb
+++ hashes.rb
@@ -1,9 +1,11 @@
 # 1. create a hash with variable name states comprised of three state names, including New York, and their abbreviations
- 
+   states = { ny: "new york", ca: 'california', il: 'Illinois' }
 # 2. create second hash with variable named request with url and ip as key names
-
+  request = { url: 'something', ip: 'something else'}
 # 3. display the url in the request hash
+  request[:url]
 
 # 4. display the abbreviation for New York
-
-# 5. display whether the states hash contains the abbreviation NY
+puts states.key('new york')
+# 5. display whether the states hash contains the abbreviation NY .has_key?
+  states.has_key?(:ny)

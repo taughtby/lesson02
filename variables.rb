baa11db67c363457c3a2fbb2a3ceb959474c89e8
Darren Logue <dazzlerlogue@gmail.com>
2012-09-26 19:11:14 +0100

    answers to variables

--- variables.rb
+++ variables.rb
@@ -1,9 +1,13 @@
 # 1. declare three variables and assign them odd-integer values in a single expression
+a, b, c = 1, 3, 5
 
 
 # 2. write an expression dividing two of the Task # 1 variables, but displaying the full fractional remainder
 # Hint: turn an integer into a float
+puts c / b.to_f
 
 
 # 3. declare a name variable, assign it your name, add one of your integer variables to it, and display the result
 
+name = "daz"
+puts name + a.to_s

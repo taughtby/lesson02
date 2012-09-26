d6933bad3720d514073dcb27961c39f520642716
Darren Logue <dazzlerlogue@gmail.com>
2012-09-26 18:45:44 +0100

    anwers for maths

--- maths.rb
+++ maths.rb
@@ -4,16 +4,25 @@ b = 2
 c = 5
  
 # 1. use ** and + in a single expression to display the result 21
-
+ puts b ** a + c
  
 # 2. use * and + in a single expression to display the result 13
 
- 
+ puts  a*b + c
 # 3. using only parentheses, change expression #2 to display 28
+ puts a * (b + c)
 
  
 # 4. display the result of 2 / 5 followed by 2.0 / 5
 
+puts 2/5
+puts 2.0/5
+
 
 # 5. write four expressions, two displaying even numbers modulus 2
 #    and two displaying odd numbers modulus 2. notice a pattern?
+
+ puts 8 % 6
+ puts 20 % 18
+ puts 19 % 17
+ puts 17 % 15
